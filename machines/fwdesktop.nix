29a8df83946936aa81b909dd2274aaffa8023027
Nox <1620870+arsonox@users.noreply.github.com>
2025-12-16 22:31:52 +0100

    cleanup

--- machines/fwdesktop.nix
+++ machines/fwdesktop.nix
@@ -29,11 +29,4 @@
   ];
 
   services.xserver.videoDrivers = [ "amdgpu" ];
-  # hardware.graphics.extraPackages = with pkgs; [
-  #   amdvlk
-  # ];
-
-  # hardware.graphics.extraPackages32 = with pkgs; [
-  #   driversi686Linux.amdvlk
-  # ];
 }
