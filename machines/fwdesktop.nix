8a88ae6a2d1a1be46fc7db296727f8fbb074691e
Nox <1620870+arsonox@users.noreply.github.com>
2025-12-16 16:09:21 +0100

    power management

--- machines/fwdesktop.nix
+++ machines/fwdesktop.nix
@@ -10,14 +10,16 @@
   #   system = "x86_64-linux";
   # };
   
-  powerManagement.enable = true;
-  # services.auto-cpufreq.enable = true;
-  # services.auto-cpufreq.settings = {
-  #   charger = {
-  #     governor = "performance";
-  #     turbo = "auto";
-  #   };
-  # };
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    cpuFreqGovernor = "schedutil";
+  };
+
+  hardware.system76.power-daemon.enable = true;
+  services.system76-scheduler.enable = true;
+
+  #services.auto-cpufreq.enable = true;
 
   boot.initrd.kernelModules = [ "amdgpu" ];
 
