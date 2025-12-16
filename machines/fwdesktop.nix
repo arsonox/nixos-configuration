ec02f1f92967755f86b68cde11f6655facd8dfef
Nox <1620870+arsonox@users.noreply.github.com>
2025-12-16 13:00:12 +0100

    some initial config for fwdesktop

--- machines/fwdesktop.nix
+++ machines/fwdesktop.nix
@@ -3,7 +3,32 @@
 {
   networking.hostName = "fwdesktop";
 
+  nixpkgs.hostPlatform = {
+    gcc.arch = "znver5";
+    gcc.tune = "znver5";
+    system = "x86_64-linux";
+  };
+
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
+
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
   imports = [
     ../configuration.nix
+  ];
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  hardware.graphics.extraPackages = with pkgs; [
+    amdvlk
+  ];
+
+  hardware.graphics.extraPackages32 = with pkgs; [
+    driversi686Linux.amdvlk
   ];
 }
--- machines/nixos.nix
+++ machines/nixos.nix
@@ -3,6 +3,29 @@
 {
   networking.hostName = "nixos";
 
+  nix.settings.substitute = false;
+  # system.includeBuildDependencies = true;
+
+  # nix.settings.system-features = [
+  #   "benchmark"
+  #   "big-parallel"
+  #   "nixos-test"
+  #   "kvm"
+  #   "gccarch-znver3"
+  # ];
+  
+  # nixpkgs.hostPlatform = {
+  #   gcc.arch = "znver3";
+  #   gcc.tune = "znver3";
+  #   system = "x86_64-linux";
+  # };
+ 
+  # nixpkgs.buildPlatform = {
+  #   gcc.arch = "znver3";
+  #   gcc.tune = "znver3";
+  #   system = "x86_64-linux";
+  # };
+
   imports = [
     ../configuration.nix
   ];
