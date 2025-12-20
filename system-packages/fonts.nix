a6927515ff9dc49812e6ae0b98ed5dfed47463d8
Nox <1620870+arsonox@users.noreply.github.com>
2025-12-20 11:45:25 +0100

    add cjk fonts

--- system-packages/fonts.nix
+++ system-packages/fonts.nix
@@ -3,9 +3,13 @@
   fonts.packages = with pkgs; [
     corefonts
     noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
     # noto-fonts-color-emoji
     twemoji-color-font
     vista-fonts
     open-sans
+    dejavu_fonts
+    ipafont
   ];
 }
