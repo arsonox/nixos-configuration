53b1a99587908552fdca16156fcd4bdbaa6cf7e3
Nox <1620870+arsonox@users.noreply.github.com>
2025-12-31 00:01:24 +0100

    fix typo

--- system-packages/ffmpeg.nix
+++ system-packages/ffmpeg.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }:
 {
-<<<<<<< HEAD
   environment.systemPackages = [
-=======
-  environment.systemPackage = [
->>>>>>> refs/remotes/origin/main
     (pkgs.ffmpeg-full.override {
       withUnfree = true;
     })
