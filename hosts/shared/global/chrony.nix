17e7e5e1821b636d75aaea9ff59bd46d1ec89bdf
Nox <1620870+arsonox@users.noreply.github.com>
2026-01-12 15:15:18 +0100

    add chrony

---
+++ hosts/shared/global/chrony.nix
@@ -0,0 +1,18 @@
+{
+  ...
+}:
+
+{
+  services.chrony = {
+    enable = true;
+    enableNTS = true;
+    servers = [
+      "time.cloudflare.com iburst nts"
+      "ntppool1.time.nl iburst nts"
+      "nts.netnod.se iburst nts"
+      "ptbtime1.ptb.de iburst nts"
+      "time.dfm.dk iburst nts"
+      "time.cifelli.xyz iburst nts"
+    ];
+  };
+}
