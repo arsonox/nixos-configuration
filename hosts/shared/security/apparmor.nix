c8dbb59b1a4951075d3179bde0276ad69a54fc32
Nox <1620870+arsonox@users.noreply.github.com>
2026-01-14 15:18:04 +0100

    further hardening add the firejail-default policy to apparmor (as complain) add firejail profiles for:   - discord   - gajim   - mpv   - telegram

--- hosts/shared/security/apparmor.nix
+++ hosts/shared/security/apparmor.nix
@@ -11,5 +11,16 @@
       apparmor-utils
       firejail
     ];
+    policies = {
+      "firejail-default" = {
+        # For now just complain instead of enforce. Firefox is broken with
+        # enforce.
+        # TODO: find out what is broken with Firefox and change to enforce.
+        state = "complain";
+        profile = ''
+          include "${pkgs.firejail}/etc/apparmor.d/firejail-default"
+        '';
+      };
+    };
   };
 }
