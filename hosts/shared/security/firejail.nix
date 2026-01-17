c1706dfcec9b3f30e111227603772c67a10c3e0f
Nox <1620870+arsonox@users.noreply.github.com>
2026-01-17 22:32:20 +0100

    add pictures folder to discord whitelist

--- hosts/shared/security/firejail.nix
+++ hosts/shared/security/firejail.nix
@@ -14,6 +14,7 @@ let
     mkdir ''${HOME}/.config/discord
     whitelist ''${HOME}/.config/discord
     whitelist ''${HOME}/Downloads
+    whitelist ''${HOME}/Pictures
 
     # Network access
     netfilter
