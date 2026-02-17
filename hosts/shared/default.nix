19b4a8410fde802736fe354aa6e796f6cc5701ef
Nox <1620870+arsonox@users.noreply.github.com>
2026-02-17 19:41:14 +0100

    add nix gc

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -143,6 +143,12 @@ in
     ];
   };
 
+  nix.gc = {
+    automatic = true;
+    dates = [ "weekly" ];
+    options = "--delete-older-than 7d";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
