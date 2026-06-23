a2f9ee644406700734d3fb8b58c3be7a1f3feac3
Nox <1620870+arsonox@users.noreply.github.com>
2026-06-23 18:21:07 +0200

    plm: fix configuration

--- hosts/shared/global/plasma-login-manager.nix
+++ hosts/shared/global/plasma-login-manager.nix
@@ -5,8 +5,8 @@
 {
   services.displayManager.plasma-login-manager.enable = true;
 
-  environment.etc."xdg/plasmaloginrc".text = ''
-    [Greeter]
-    Background=${../../../wallpapers/castorice-purple.jpg}
+  environment.etc."plasmalogin.conf".text = ''
+    [Greeter][Wallpaper][org.kde.image][General]
+    Image=file://${../../../wallpapers/castorice-purple.jpg}
   '';
 }
